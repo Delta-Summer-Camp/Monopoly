0f7e57419ebd3c01a4d0769bd902d2e9ca5240e2
Dmitry <d.ablov@gmail.com>
2022-03-27 18:01:25 +0200

    Итоги занятия 27 марта (с исправленной ошибкой, см. комменты)

--- JS/auth.mjs
+++ JS/auth.mjs
@@ -1,14 +1,47 @@
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.9/firebase-auth.js";
+import { getAuth, onAuthStateChanged, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.6.9/firebase-auth.js";
+import { getDatabase, get, ref, child, set } from "https://www.gstatic.com/firebasejs/9.6.9/firebase-database.js";
+var myUID = null;
 
 function auth(app) {
 	return new Promise(function(resolve, reject){
 		let param = "This session is not authorized!";
 		const fbAuth = getAuth(app);
 
-		if (fbAuth) {
-			param = "We are authrized!";
-			resolve(param);
-		}
+		// check if the authorization has been changed
+		onAuthStateChanged(fbAuth, function(user) {
+			// ...if not authorized
+			if (!user) {
+			const provider = new GoogleAuthProvider();
+
+			signInWithPopup(fbAuth, provider)
+				.catch(function(error) {
+					alert(error.message);
+				})				
+			}
+
+			// ...if authorized
+			else {
+				myUID = user.uid;
+				const dbRef = ref(getDatabase(app));
+				get(child(dbRef, 'users/' + myUID))
+					.then(function(snapshon) {
+						if (snapshon.exists) {
+							const currentPlayer = snapshon.val();
+							if(currentPlayer && currentPlayer.ninkname) {
+								param = "This session is authorized as " + currentPlayer.ninkname;
+								resolve(param);
+							}
+							else {
+								const myName = prompt("Привет! Как тебя зовут?", "Captain Nemo");
+								// Ошибка была тут: забыли импортировать функцию 'set()'!
+								set(child(dbRef, 'users' + myUID), myName);
+								param = "This session is authorized as " + myName;
+								resolve(param);
+							}
+						}
+					})
+			}
+		})
 	})
 }
 
