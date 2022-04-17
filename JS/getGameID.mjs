7d6d286aea6240f9a2fb70d60afb3f04dd1cd118
Dmitry <d.ablov@gmail.com>
2022-04-17 17:10:16 +0200

    Текст к занятию 17 апреля

---
+++ JS/getGameID.mjs
@@ -0,0 +1,61 @@
+/*
+*	Getting gameID from URL
+*/
+
+import { getDatabase, get, ref, child, set } from "https://www.gstatic.com/firebasejs/9.6.9/firebase-database.js";
+import { app } from "./initFirebase.mjs";
+import { myUID } from "./auth.mjs";
+
+let gameID = null;
+
+function getGameID() {
+	return new Promise((resolve, reject) => {
+		// read URL Params
+		const params = new URLSearchParams(location.search.substring(1));
+		gameID = parseInt(params.get("gameID"), 10);
+
+		// if there is not gameID...
+		if (!gameID) {
+			gameID = prompt("Введи идентификатор игры чтобы присоединиться\n" +
+				"или ноль, чтобы начать новую игру", 0);
+		}
+
+		// ...if zero:
+		if (gameID == 0) {
+			gameID = Date.now();
+
+			const gameRef = child(ref(getDatabase(app)), 'games/' + gameID);
+			set(gameRef, {
+				status: "New",
+				owner: myUID
+			})
+			.then(() => {
+				location.href = '/?gameID=' + gameID;
+			});
+		}
+
+		// ...otherways check if the game exists and active
+		else {
+			const gameRef = child(ref(getDatabase(app)), 'games/' + gameID);
+			get(child(gameRef, '/status')).then(snapshot => {
+				if (snapshot.exists()) {
+					const gameStatus = snapshot.val();
+					switch (gameStatus) {
+						case "New": // new game - Ok
+							resolve(gameID);
+							break;
+						default: // something wrong
+							alert("Неизвестный статус игры");
+							reject();
+					}
+				}
+				else {
+					alert("Что-то пошло не так");
+					reject();
+				}
+			})
+		}
+	});
+}
+
+export { getGameID }
